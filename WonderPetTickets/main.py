e7b34ab746ba33ff015065f0be2dc5f4e61551a0
clarkrayl7-bit <clarkrayl7@gmail.com>
2026-09-25 10:37:48 -0600

    Add main function to process ticket sales data

--- WonderPetTickets/main.py
+++ WonderPetTickets/main.py
@@ -2,3 +2,8 @@ import config, utils
 import pandas as pd
 
 df = pd.read_csv('cinema_hall_ticket_sales.csv')
+if __name__ = "__main__":
+  df = utils.prepare_df(df)
+  df = utils.sort_age_type(df)
+  display(df)
+  utils.flag_nulls(df)
